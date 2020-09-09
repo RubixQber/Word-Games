daaee54a82ef30bbe19bb8210a54696741fd8d75
RubixQber <joshua.lowe@menloschool.org>
2020-09-09 15:17:40 -0700

    started ghost

--- word_games.py
+++ word_games.py
@@ -1,1 +1,17 @@
-print(68419)
+file = open("words.txt", "r")
+word_list = []
+for line in file:
+    word_list += line.lower().split()
+
+word = ""
+player = 0
+while word.lower() not in word_list:
+    player = player % 2 + 1
+    print("Enter next letter: ")
+    letter = input()
+    word += letter
+    print("The current fragment is: " + word)
+if player == 1:
+    print("player 2 won")
+else:
+    print("player 1 won")
